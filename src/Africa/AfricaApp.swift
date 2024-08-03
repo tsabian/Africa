fc5d9b17b58a22abf2a100b5e8d179339e39ebb9
Tiago Oliveira <tsabian@hotmail.com>
2024-08-03 19:15:02 -0300

    Configure project and include assets, colors, videos, and data files

---
+++ src/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Tiago de Oliveira on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
