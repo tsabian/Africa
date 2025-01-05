3606483450d31804ca1362af0e295d3c3477cd5e
Tiago Oliveira <tsabian@hotmail.com>
2025-01-05 19:16:19 -0300

    developtment

---
+++ src/Africa/App/Scenes/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Tiago de Oliveira on 14/08/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("MapView")
+    }
+}
+
+#Preview {
+    MapView()
+}
---
+++ src/Africa/Components/CoverImageView.swift
@@ -0,0 +1,28 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by Tiago de Oliveira on 14/08/24.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    var body: some View {
+        TabView {
+            ForEach(0 ..< 5) { item in
+                Image(.coverLion)
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+struct CoverImageView_Preview: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+    }
+}
