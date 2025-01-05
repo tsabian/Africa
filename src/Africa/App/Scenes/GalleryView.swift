6d343c2465c200c5cf788ba9999bbd2c4478e69c
Tiago Oliveira <tsabian@hotmail.com>
2025-01-05 19:16:19 -0300

    developtment

---
+++ src/Africa/App/Scenes/GalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryView.swift
+//  Africa
+//
+//  Created by Tiago de Oliveira on 14/08/24.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text("GalleryView")
+    }
+}
+
+#Preview {
+    GalleryView()
+}
