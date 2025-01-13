b8000034395e8dad76f4ed1fc9f2c1929ae52f42
Tiago de Oliveira <tiago@Tiagos-Mac-mini.local>
2025-01-13 17:53:59 -0300

    xcodegen added

---
+++ src/Scripts/lint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if command -v swiftlint >/dev/null 2>&1
+then
+    swiftlint
+else
+    echo "warning: `swiftlint` command not found - See https://github.com/realm/SwiftLint#installation for installation instructions."
+fi
